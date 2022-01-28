af0745463ee9ee235d5721636f65c49958a341fe
deepindersinghbti <85491004+deepindersinghbti@users.noreply.github.com>
2022-01-28 20:01:40 +0530

    Update sketch.js
    Players can now press the SPACE key to reset the crumpled ball and get another chance to hit it into the dustbin.

--- sketch.js
+++ sketch.js
@@ -1,54 +1,34 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+class SlingShot {
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
 
-var engine, world;
+      stiffness: 0.04,
+      length: 10,
+    };
+    this.slingshot = Constraint.create(options);
+    this.pointB = pointB;
+    World.add(world, this.slingshot);
+  }
+  display() {
+    if (this.slingshot.bodyA !== null) {
+      var posA = this.slingshot.bodyA.position;
+      var posB = this.pointB;
 
-var ground;
-var block1, block2, block3;
-var paper;
-var dustbin;
-var slingshot;
+      push();
+      strokeWeight(3);
+      stroke("Black");
+      line(posA.x, posA.y, posB.x, posB.y);
+      pop();
+    }
+  }
 
-function setup() {
-  createCanvas(1500, 400);
+  fly() {
+    this.slingshot.bodyA = null;
+  }
 
-  engine = Engine.create();
-  world = engine.world;
-
-  ground = new Block(980, 390, 1960, 20, "Brown");
-
-  block1 = new Block(1060, 300, 10, 150, "Green");
-  block2 = new Block(1140, 370, 150, 10, "Green");
-  block3 = new Block(1220, 300, 10, 150, "Green");
-
-  dustbin = new Dustbin(1140, 300, 200, 160);
-  paper = new Paper(200, 200);
-  slingshot = new SlingShot(paper.body, { x: 200, y: 100 });
-}
-
-function draw() {
-  background(255);
-
-  Engine.update(engine);
-
-  ground.display();
-
-  block1.display();
-  block2.display();
-  block3.display();
-
-  dustbin.display();
-
-  slingshot.display();
-  paper.display();
-}
-
-function mouseDragged() {
-  Matter.Body.setPosition(paper.body, { x: mouseX, y: mouseY });
-}
-
-function mouseReleased() {
-  slingshot.fly();
+  attach(body) {
+    this.slingshot.bodyA = body;
+  }
 }
