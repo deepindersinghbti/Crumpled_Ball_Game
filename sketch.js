2a98d424350a7605c8e93c0e147b03595101f8ba
deepindersinghbti <85491004+deepindersinghbti@users.noreply.github.com>
2022-01-28 20:06:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,60 @@
-class SlingShot {
-  constructor(bodyA, pointB) {
-    var options = {
-      bodyA: bodyA,
-      pointB: pointB,
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-      stiffness: 0.04,
-      length: 10,
-    };
-    this.slingshot = Constraint.create(options);
-    this.pointB = pointB;
-    World.add(world, this.slingshot);
-  }
-  display() {
-    if (this.slingshot.bodyA !== null) {
-      var posA = this.slingshot.bodyA.position;
-      var posB = this.pointB;
+var engine, world;
 
-      push();
-      strokeWeight(3);
-      stroke("Black");
-      line(posA.x, posA.y, posB.x, posB.y);
-      pop();
-    }
-  }
+var ground;
+var block1, block2, block3;
+var paper;
+var dustbin;
+var slingshot;
 
-  fly() {
-    this.slingshot.bodyA = null;
-  }
+function setup() {
+  createCanvas(2000, 400);
 
-  attach(body) {
-    this.slingshot.bodyA = body;
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Block(980, 390, 1960, 20, "Brown");
+
+  block1 = new Block(1200, 300, 10, 150, "Green");
+  block2 = new Block(1280, 370, 150, 10, "Green");
+  block3 = new Block(1360, 300, 10, 150, "Green");
+
+  dustbin = new Dustbin(1280, 300, 200, 160);
+  paper = new Paper(200, 200);
+  slingshot = new SlingShot(paper.body, { x: 200, y: 100 });
+}
+
+function draw() {
+  background(255);
+
+  Engine.update(engine);
+
+  ground.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+
+  dustbin.display();
+
+  slingshot.display();
+  paper.display();
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(paper.body, { x: mouseX, y: mouseY });
+}
+
+function mouseReleased() {
+  slingshot.fly();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    slingshot.attach(paper.body);
   }
 }
